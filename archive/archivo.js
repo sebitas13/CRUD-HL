d232ffccd09e85b4fb688e44fae1bfccf43acc8a
sebas <jesus.huamanculi1@unmsm.edu.pe>
2023-08-25 21:30:47 -0500

    Updates

--- archive/archivo.js
+++ archive/archivo.js
@@ -16,7 +16,7 @@ listaArmas.agregarArma(
     "nothing",
     "Half life 1",
     "Basic weapon, useful on close distances",
-    "../images/armas/crowbar.png"
+    "../images/armas/crowbar.webp"
   )
 );
 listaArmas.agregarArma(
@@ -27,7 +27,7 @@ listaArmas.agregarArma(
     "fires faster but with less accuraccy",
     "Half life 1",
     "Default weapon. Try aim for the head with primary fire. It’s funcional underwater.",
-    "../images/armas/pistol.png"
+    "../images/armas/pistol.webp"
   )
 );
 listaArmas.agregarArma(
@@ -38,7 +38,7 @@ listaArmas.agregarArma(
     "fires two shots at once, deadly in close distance",
     "Half life 1",
     "Powerful weapon, especially in close distance.",
-    "../images/armas/shotgun.png"
+    "../images/armas/shotgun.webp"
   )
 );
 listaArmas.agregarArma(
@@ -62,7 +62,7 @@ listaArmas.agregarArma(
     "zoom in",
     "Half life 1",
     "It is better to fire unzoomed. It isn’t completely accurate even if you zoom in.",
-    "../images/armas/magnum.png"
+    "../images/armas/magnum.webp"
   )
 );
 
@@ -74,7 +74,7 @@ listaArmas.agregarArma(
     "throws grenade",
     "Half life 1",
     "If you pick up this weapon it isn’t fully loaded so it’s better to reaload immediatly.",
-    "../images/armas/mp5.png"
+    "../images/armas/mp5.webp"
   )
 );
 
@@ -86,7 +86,7 @@ listaArmas.agregarArma(
     "switch laser dot on or off",
     "Half life 1",
     "Fires guided rocket when laser is on, when is switched off rocket goes directly where you aimed. Can be used underwater.",
-    "../images/armas/rpg.png"
+    "../images/armas/rpg.jpg"
   )
 );
 
@@ -98,7 +98,7 @@ listaArmas.agregarArma(
     "charges gauss for bigger damage and you can use it for flying.",
     "Half life 1",
     "Best weapon in game. You can shoot through walls with it. It will do area damage behind wall (this is called wallgauss). Can be used for flying, dodging or reaching higher places.",
-    "../images/armas/gauss.png"
+    "../images/armas/gauss.webp"
   )
 );
 
@@ -110,7 +110,7 @@ listaArmas.agregarArma(
     "nothing",
     "Half life 1",
     "Very powerful weapon, but many players considers it as a noob gun. Forbidden on my server.",
-    "../images/armas/gluon.png"
+    "../images/armas/gluon.webp"
   )
 );
 
@@ -122,7 +122,7 @@ listaArmas.agregarArma(
     "faster, but bees fly only straight forward",
     "Half life 1",
     "Alien hornet gun. Regenerating ammo automaticly up to 12 bees. Works underwater too.",
-    "../images/armas/hornet.png"
+    "../images/armas/hornet.jpg"
   )
 );
 
@@ -134,7 +134,7 @@ listaArmas.agregarArma(
     "nothing",
     "Half life 1",
     "Can be placed everywhere on solid material.",
-    "../images/armas/tripmine.png"
+    "../images/armas/tripmine.jpeg"
   )
 );
 
@@ -165,18 +165,18 @@ listaCriaturas.agregarCriatura(
 
 
 export const imagenesArmas = [
-  "../images/armas/crossbow.png",
-  "../images/armas/crowbar.png",
-  "../images/armas/gauss.png",
-  "../images/armas/gluon.png",
-  "../images/armas/snark.png",
-  "../images/armas/grenade.png",
-  "../images/armas/hornet.png",
-  "../images/armas/magnum.png",
-  "../images/armas/mp5.png",
-  "../images/armas/pistol.png",
-  "../images/armas/shotgun.png",
-  "../images/armas/tripmine.png",
+  "../images/armas/crossbow.jpg",
+  "../images/armas/crowbar.webp",
+  "../images/armas/gauss.webp",
+  "../images/armas/gluon.webp",
+  "../images/armas/snark.webp",
+  "../images/armas/grenade.jpeg",
+  "../images/armas/hornet.jpg",
+  "../images/armas/magnum.webp",
+  "../images/armas/mp5.webp",
+  "../images/armas/pistol.webp",
+  "../images/armas/shotgun.webp",
+  "../images/armas/tripmine.jpeg",
 
 ]
 
